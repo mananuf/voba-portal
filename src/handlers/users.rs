4d3d4a50dd5a5fc8c8f366ec01c249c43b2ac947
mananuf <bmananaf@gmail.com>
2025-07-26 18:58:52 +0100

    chore: adds default test route for project

---
+++ src/handlers/users.rs
@@ -0,0 +1,12 @@
+use crate::{database::connection::DbPool, models::user::User, utils::helpers::ApiResponse};
+use actix_web::{web, HttpResponse, Result};
+use tracing::error;
+
+pub async fn index(pool: web::Data<DbPool>) -> Result<HttpResponse> {
+    let users = User::find_all(&pool).await.map_err(|e| {
+        error!("Failed to fetch users: {}", e);
+        actix_web::error::ErrorInternalServerError("Failed to fetch users")
+    })?;
+
+    Ok(HttpResponse::Ok().json(ApiResponse::success(users)))
+}
